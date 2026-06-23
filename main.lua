1f4470bb2457254f96bdee5cc0a2fc195ffd9499
Isaac Lara <eng.isaaclara@gmail.com>
2026-06-22 20:25:16 -0400

    Assets agregados v.1

--- main.lua
+++ main.lua
@@ -13,6 +13,11 @@ end
 
 function love.load()
 
+    love.graphics.setDefaultFilter(
+        "nearest",
+        "nearest"
+    )
+
     player = {
         x = 380,
         y = 280,
@@ -26,31 +31,138 @@ function love.load()
         isCharging = false,
         chargeTime = 0,
         facing = 1,
+        spriteOffsetX = 13,
+        spriteOffsetY = -70
+        
         
     }
-    
-    map = sti("maps/room1.lua")
 
-    for name, layer in pairs(map.layers) do
-        print("CAPA:", name)
-        print("TIPO:", layer.type)
+
+    player.animations = {}
+
+    player.animations.idle = {
+        image = love.graphics.newImage("assets/player/idle.png"),
+        frames = {},
+        currentFrame = 1,
+        timer = 0,
+        speed = 0.15
+    }
+
+    player.animations.charge = {
+        image = love.graphics.newImage("assets/player/idle.png"),
+        frames = {},
+        currentFrame = 1,
+        timer = 0,
+        speed = 0.15
+    }
+
+    player.animations.run = {
+        image = love.graphics.newImage("assets/player/run.png"),
+        frames = {},
+        currentFrame = 1,
+        timer = 0,
+        speed = 0.08
+    }
+
+    player.animations.jump = {
+        image = love.graphics.newImage("assets/player/jump.png"),
+        frames = {},
+        currentFrame = 1,
+        timer = 0,
+        speed = 0.2
+    }
+
+    player.animations.turnaround = {
+        image = love.graphics.newImage("assets/player/run-turnaround.png"),
+        frames = {},
+        currentFrame = 1,
+        timer = 0,
+        speed = 0.06
+    }
+
+
+    local function generateFrames(animation, frameCount)
+
+        for i = 0, frameCount - 1 do
+
+                table.insert(
+                    animation.frames,
+
+                    love.graphics.newQuad(
+                        i * 80,
+                        0,
+                        80,
+                        80,
+                        animation.image:getDimensions()
+                    )
+                )
+
+         end
+
     end
+
+    
+
+    generateFrames(player.animations.idle, 18)
+    generateFrames(player.animations.charge, 18)
+    generateFrames(player.animations.run, 24)
+    generateFrames(player.animations.jump, 17)
+    generateFrames(player.animations.turnaround, 5)
+
+    setState("idle")
+    setState("run")
+    setState("jump")
+
+
+    rooms = {
+        "maps/room1.lua",
+        "maps/room2.lua",
+        "maps/room3.lua"
+    }
+
+        roomBackgrounds = {
+        [1] = "forest",
+        [2] = "forest",
+        [3] = "forest"
+    }
+
+    currentRoom = 1
 
     platforms = {}
+    
+    function loadRoom(index)
 
-    local layer = map.layers["Collisions"]
+        map = sti(rooms[index])
 
-    if layer then
-        for _, obj in ipairs(layer.objects) do
-            table.insert(platforms, {
-                x = obj.x,
-                y = obj.y,
-                width = obj.width,
-                height = obj.height
-            })
+        for name, layer in pairs(map.layers) do
+            print("CAPA:", name)
+            print("TIPO:", layer.type)
         end
+
+        platforms = {}
+
+        local layer = map.layers["Collisions"]
+
+        if layer then
+            for _, obj in ipairs(layer.objects) do
+                table.insert(platforms, {
+                    x = obj.x,
+                    y = obj.y,
+                    width = obj.width,
+                    height = obj.height
+                })
+            end
+        end
+
+        roomHeight = map.height * map.tileheight
+
+        loadBackground(
+            roomBackgrounds[index]
+        )
+
     end
 
+    loadRoom(currentRoom)
 
     verticalMultiplier = 1
     horizontalMultiplier = 0.7
@@ -59,34 +171,94 @@ function love.load()
     gravity = 2000
     maxChargeTime = 1
 
+    
+end
 
-    roomWidth = 800
-    roomHeight = 600
+function loadBackground(backgroundName)
 
-    maxRooms = 3
-    currentRoom = 1
+    backgrounds = {}
 
-    rooms = {
+    for i = 1, 6 do
 
-        [1] = {
-            {x = 0, y = 550, width = 800, height = 50},
-            {x = 150, y = 450, width = 120, height = 20}
-        },
+        backgrounds[i] =
+            love.graphics.newImage(
+                "assets/backgrounds/" ..
+                backgroundName ..
+                "/background" ..
+                i ..
+                ".png"
+            )
 
-        [2] = {
-            {x = 200, y = 550, width = 400, height = 50},
-            {x = 100, y = 350, width = 120, height = 20}
-        }
+    end
 
-    }
+end
+
+function updateAnimation(dt)
+
+    local anim = player.animations[player.state]
+
+    if #anim.frames <= 1 then
+        return
+    end
+
+    anim.timer = anim.timer + dt
+
+    if anim.timer >= anim.speed then
+
+        anim.timer = anim.timer - anim.speed  -- ✅ FIX
+
+        anim.currentFrame = anim.currentFrame + 1
+
+        if anim.currentFrame > #anim.frames then
+            anim.currentFrame = 1
+        end
+
+    end
+end
+
+function setState(newState)
+
+    if player.state ~= newState then
+
+        player.state = newState
+
+        local anim = player.animations[newState]
+
+        anim.currentFrame = 1
+        anim.timer = 0
+
+        print("Estado:", newState)
+
+    end
 
 end
 
 function love.update(dt)
 
 
+    updateAnimation(dt)
     player.previousX = player.x
     player.previousY = player.y
+
+
+    if player.isCharging then
+
+            setState("charge")
+
+        elseif not player.isGrounded then
+
+            setState("jump")
+
+        elseif love.keyboard.isDown("a")
+            or love.keyboard.isDown("d") then
+
+            setState("run")
+
+        else
+
+            setState("idle")
+
+    end
 
     if player.isGrounded 
         and not player.isCharging then
@@ -214,24 +386,60 @@ function love.update(dt)
         player.chargeTime = maxChargeTime
     end
 
+
+
+    -- subir de room
     if player.y + player.height < 0 then
 
-        if currentRoom > 1 then
-            currentRoom = currentRoom - 1
-        end
+        if currentRoom < #rooms then
 
-        player.y = roomHeight - player.height - 10
+            currentRoom = currentRoom + 1
+
+            loadRoom(currentRoom)
+
+            player.y = 600 - player.height - 10
+
+        else
+
+            player.y = 0
+
+        end
 
     end
 
-    if player.y > roomHeight then
+    -- bajar de room
+    if player.y > 600 then
 
-        if currentRoom < maxRooms then
-            currentRoom = currentRoom + 1
+        if currentRoom > 1 then
+
+            currentRoom = currentRoom - 1
+
+            loadRoom(currentRoom)
+
+            player.y = 10
+
+        else
+
+            player.y = 600 - player.height
+
         end
 
-        player.y = 0
+    end
 
+    if love.keyboard.isDown("up") then
+        player.spriteOffsetY = player.spriteOffsetY - 1
+    end
+
+    if love.keyboard.isDown("down") then
+        player.spriteOffsetY = player.spriteOffsetY + 1
+    end
+
+    if love.keyboard.isDown("left") then
+        player.spriteOffsetX = player.spriteOffsetX - 1
+    end
+
+    if love.keyboard.isDown("right") then
+        player.spriteOffsetX = player.spriteOffsetX + 1
     end
 
 end
@@ -283,19 +491,38 @@ end
 
 function love.draw()
 
+   for i = 1, 6 do
+
+        love.graphics.draw(
+            backgrounds[i],
+            0,
+            0
+        )
+
+    end
+
     map:draw()
 
     love.graphics.print("Vertical Edge", 10, 10)
 
 
+    local anim =
+        player.animations[player.state]
 
-    love.graphics.rectangle(
-        "fill",
-        player.x,
-        player.y,
-        player.width,
-        player.height
+    local scale = 2
+
+    love.graphics.draw(
+        anim.image,
+        anim.frames[anim.currentFrame],
+        player.x + player.spriteOffsetX,
+        player.y + player.spriteOffsetY,
+        0,
+        player.facing * scale,
+        scale,
+        30,
+        0
     )
+    
 
     love.graphics.print(
         "En suelo: " .. tostring(player.isGrounded),
@@ -336,4 +563,45 @@ function love.draw()
         110
     )
 
+    love.graphics.setColor(1,0,0)
+        for _, p in ipairs(platforms) do
+            love.graphics.rectangle("line", p.x, p.y, p.width, p.height)
+        end
+        love.graphics.setColor(1,1,1)
+
+
+    love.graphics.print(
+        "Estado: " .. player.state,
+        10,
+        130
+    )
+
+        --[love.graphics.setColor(0,1,0)
+        --love.graphics.rectangle(
+           -- "line",
+           -- player.x,
+           -- player.y,
+           -- player.width,
+           -- player.height
+        --)
+        love.graphics.setColor(1,1,1)
+
+        love.graphics.print(
+            "Offset X: "..player.spriteOffsetX,
+            10,
+            150
+        )
+
+        love.graphics.print(
+            "Offset Y: "..player.spriteOffsetY,
+            10,
+            170
+        )
+
+        love.graphics.print(
+            "Facing: " .. player.facing,
+            10,
+            190
+        )
+    
 end
